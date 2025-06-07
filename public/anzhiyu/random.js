08bbb99f3c602f0b7b75bc0e95af08bab9372640
937bb <3535025404@qq.com>
2025-06-07 09:41:36 +0800

    增加百度统计代码

--- public/anzhiyu/random.js
+++ public/anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/06/06/Docker常用命令1/","2025/06/06/GIT常用命令1/","2025/06/06/Linxu常用命令1/"];function toRandomPost(){
+var posts=["2025/06/06/GIT常用命令1/","2025/06/06/Docker常用命令1/","2025/06/06/Linxu常用命令1/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
